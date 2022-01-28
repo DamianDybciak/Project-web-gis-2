8ab02564a305315fdac22d8e0b256908e46c4cf4
Damian Dybciak <damian.dybciak00@gmail.com>
2022-01-28 01:05:56 +0100

    Dodanie widgetów oraz popup

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,21 +1,28 @@
 require([
     "esri/Map",
     "esri/views/SceneView",
-    "dijit/form/Button",
     "esri/layers/FeatureLayer",
-    "esri/Graphic",
-    "esri/layers/GraphicsLayer",
     "esri/widgets/BasemapGallery",
-    "esri/widgets/Expand"
-], (Map, SceneView,Button,FeatureLayer,Graphic,GraphicsLayer,MapGallery,Expand) =>{
-
-
+    "esri/widgets/Expand",
+    "esri/widgets/Search",
+    "esri/widgets/Measurement",
+    "esri/widgets/Legend",
+    "esri/widgets/LayerList"
+], (Map, SceneView,FeatureLayer,MapGallery,Expand,Search,Measurement,Legend,LayerList) =>{
     
+    
+    
+    const popTemplate ={
+        title:"Dane",
+        content: "Nazwa: {NazwaPunkt} <br>Adres: {ADRES}<br> Miasto: {Miasto}<br> Kod pocztowy: {KodPocztow} <br> Lokalizacja: {Lokalizacj}"
+    };
 
     const layer = new FeatureLayer({
-        url:"https://services1.arcgis.com/YmCK8KfESHdxUQgm/arcgis/rest/services/stacje_krwiodawstwa/FeatureServer",
+        url:"https://services1.arcgis.com/YmCK8KfESHdxUQgm/ArcGIS/rest/services/stacje_krwiodawstwa/FeatureServer/0",
+        popupTemplate: popTemplate,
     });
-
+    
+    
 
     
     const map1 = new Map({
@@ -33,48 +40,57 @@ require([
     });
     
     
-    const geom = {
-        type:"point",
-        longitude:21,
-        latitude: 52.15,
-        
-    };
-    
-    const symbol = {
-        type: "simple-marker",
-        style: "X",
-        size: 14,
-        color: "green",
-        
-    
-    }; 
-    
-    const attr= {
-        name : "Polska",
-        code: "POL"
-    };
-
-
-    const graph = new Graphic({
-        geometry: geom,
-        attributes : attr,
-        symbol : symbol,
-    });
     
     const basemapGallerywg = new MapGallery({
         view : view ,
     });
-
+    const searchWg = new Search({
+        view: view
+      });
+      
+    view.ui.add(searchWg, {
+          position: "top-right",
+          index: 2
+        });
+    const measurement = new Measurement({
+        view: view,
+        activeTool: "distance"
+      });
+    view.ui.add(measurement, "top-right");
+    function switchTool() {
+        const tool = measurement.activeTool === "distance" ? "area" : "distance";
+        measurement.activeTool = tool;
+       }
+       
+    let legend = new Legend({
+        view: view
+      });
+      
+    view.ui.add(legend, "bottom-right");   
+    
+    
+    let layerlist = new LayerList({
+        view: view
+    });
+   
     const expWg = new Expand({
         view:view,
         content: basemapGallerywg,
+        
     });
-
-
     view.ui.add(expWg,{position:"top-right"});
 
 
-    //graphLayer.graphics.push(graph);
+    const expWg2 = new Expand({
+        view:view,
+        content: layerlist,
+        
+    });
+    view.ui.add(expWg2,{position:"top-right"});
+
+
+
+
     
     //map1.graphics.add(graph);
 
